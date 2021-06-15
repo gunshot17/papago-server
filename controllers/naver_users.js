c64c11086b6241ac508f56ebd4f5b6df4ed65799
songi <gunshot17@naver.com>
2021-06-15 16:54:06 +0900

    master

--- controllers/naver_users.js
+++ controllers/naver_users.js
@@ -8,14 +8,14 @@ const connection = require("../db/mysql_connection");
 // @response success
 
 exports.createNaverUser = async (req, res, next) => {
-    let email = req.body.email;
-    let nickname = req.body.nickname;
+    let sns_id = req.body.email;
+    let sns_name = req.body.nickname;
     let id = req.body.id;
 
 
 
     // npm validator
-    if (!validator.isEmail(email)) {
+    if (!validator.isEmail(sns_id)) {
         res.status(400).json();
         return;
     }
@@ -23,7 +23,7 @@ exports.createNaverUser = async (req, res, next) => {
 
 
     let query = "insert into naver_info (id , sns_id, sns_name) values ( ? , ? ,? )";
-    let data = [id, email, nickname];
+    let data = [id, sns_id, sns_name];
     let user_id;
 
     const conn = await connection.getConnection();
